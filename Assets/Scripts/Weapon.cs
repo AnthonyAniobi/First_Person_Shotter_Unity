7df16e81fbe4c9876120666f1d848e7e192bb72a
AnthonyAniobi <anthonyaniobi198@gmail.com>
2026-09-23 16:29:36 +0100

    Add burst and auto shooting modes to Weapon class; introduce RedTarget prefab
    - Enhanced the Weapon class to support multiple shooting modes: Single, Burst, and Auto.
    - Implemented burst shooting with adjustable burst count and spread intensity.
    - Added functionality for resetting shooting after a delay.
    - Created a new RedTarget prefab with associated components for target practice.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,9 +10,30 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float bulletSpeed = 30f;
     [SerializeField] private float bulletLifeTime = 2f;
 
+    [SerializeField] private Camera playerCamera;
+
+    [SerializeField] private int burstCount = 3;
+    [SerializeField] private float spreadIntensity = 0.1f;
+    [SerializeField] private bool allowResetShooting = true;
+
+    private int currentBurstCount = 0;
+    [SerializeField] private float shootDelay = 0.2f;
+    
+    public ShootingMode currentShootingMode = ShootingMode.Single;
+    public bool isShooting, readyToShoot = true;
+
+    private bool canResetShooting = true;
+    
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
     void Start()
     {
-        
+        ResetShooting();
     }
 
     // Update is called once per frame
@@ -20,22 +41,82 @@ public class Weapon : MonoBehaviour
     {
         InputAction fireBullet = InputSystem.actions.FindAction("Attack");
 
-        if (fireBullet.WasPressedThisFrame())
+        
+        if(currentShootingMode == ShootingMode.Single)
         {
+            // tap to shoot
+            isShooting = fireBullet.WasPressedThisFrame();
+        }else if(currentShootingMode == ShootingMode.Burst)
+        {
+            // tap to shoot
+            isShooting = fireBullet.WasPressedThisFrame();
+        }else if(currentShootingMode == ShootingMode.Auto)
+        {
+            // Hold down to shoot
+            isShooting = fireBullet.IsPressed();
+        }
+
+        if (isShooting && readyToShoot)
+        {
+            currentBurstCount = burstCount;
             FireWeapon();
         }
     }
 
     private void FireWeapon()
     {
+        readyToShoot = false;
+        Vector3 shootingDirection = GetBulletDirection().normalized;
+
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpeed * transform.forward.normalized, ForceMode.Impulse);
+        bullet.transform.forward = shootingDirection;
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletSpeed, ForceMode.Impulse);
         StartCoroutine(RemoveBullet(bullet, bulletLifeTime));
+
+        if (allowResetShooting)
+        {
+            Invoke("ResetShot", shootDelay);
+            allowResetShooting = false;
+        }
+        // Burst Mode
+        if(currentShootingMode == ShootingMode.Burst && currentBurstCount > 1)
+        {
+            currentBurstCount--;
+            Invoke("FireWeapon", shootDelay);
+        }
     }
 
     private IEnumerator RemoveBullet(GameObject bullet, float delay)
     {
         yield return new WaitForSeconds(delay);
         Destroy(bullet);
+    }
+
+    private Vector3 GetBulletDirection()
+    {   
+        // raycast from the center of the camera
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        
+        Vector3 targetPoint;
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100f);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawnPoint.position;
+        float x = Random.Range(-spreadIntensity, spreadIntensity);
+        float y = Random.Range(-spreadIntensity, spreadIntensity);
+        direction += new Vector3(x, y, 0f);
+        return direction;
+    }
+
+    private void ResetShooting()
+    {
+        readyToShoot = true;
+        allowResetShooting = true;
     }
 }
