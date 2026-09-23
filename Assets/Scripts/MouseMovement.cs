ffa0ce47a687a817a699792865a6284f4b042dbe
AnthonyAniobi <anthonyaniobi198@gmail.com>
2026-09-23 11:15:36 +0100

    reorder project folders

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
