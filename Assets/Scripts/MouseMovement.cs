ce2400888313c1f8c6b593c899cd356fad84f9c2
AnthonyAniobi <anthonyaniobi198@gmail.com>
2026-09-23 11:48:02 +0100

    setup mousemovement

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseMovement : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
-    public 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float mouseSensitivity = 19f;
+    [SerializeField] private float topClamp = 90f;
+    [SerializeField] private float bottomClamp = 90f;
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+    
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       InputAction mouseDelta = InputSystem.actions.FindAction("Look");
+       /// mouseX is for rotation up and down
+       float mouseX = mouseDelta.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
+       /// mouseY is for rotation left and right
+       float mouseY = mouseDelta.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+
+       rotationX += mouseY;
+       rotationX = Mathf.Clamp(rotationX, -topClamp, bottomClamp);
+       rotationY += mouseX;
+
+       transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
     }
 }
